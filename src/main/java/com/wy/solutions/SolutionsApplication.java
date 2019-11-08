ca6e10f18194095e946825111d9ec19431b708fc
wangye <wangye@joinboom.cn>
2019-11-08 14:43:45 +0800

    init

---
+++ src/main/java/com/wy/solutions/SolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.wy.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolutionsApplication.class, args);
+    }
+
+}
